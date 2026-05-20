781d837043c1d2040e149fa9df2f39321e5af446
Nayraiyt <naomimorganc@gmail.com>
2026-05-20 11:03:42 -0400

    Fixed Migration Bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Login from './routes/Login/Login.tsx';
+import Past from './routes/Past/Past.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Login />
+    <Past/>
   </StrictMode>,
 )
